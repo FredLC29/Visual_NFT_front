b2a2cef91f0401ddea93d2b465854a5bf6a73b24
Marjorie Ngoupende <marjorie.ngoupende@gmail.com>
2021-06-30 14:54:30 +0200

    fix css & html

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import egg from './images/background.jpg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import logo from '../images/background.jpg'
 
 import { useState, useCallback } from 'react'
 
@@ -21,45 +23,47 @@ function App() {
       }
     },[]
   )
+  
 
   return (
-    <div className="App">
+          <div className="App">
       <header className="App-header">
-        <h1>The NFT Easter Egg Hunt 2022</h1>
-        <h2>Earn your NFTs!</h2>
+      {
+          isConnectedWeb3
+            ? <p><img src="https://cdn.worldvectorlogo.com/logos/metamask.svg" alt="logo_metamask" class="logo"></img></p>
+            : <button className="btn btn-primary btn-lg" onClick={connectToWeb3}>Connect here</button>
+        }
+        <h1 className="App-title1">The NFT Easter Egg Hunt 2022</h1>
+        <h2 className="App-title2">Earn your NFTs!</h2>
 
-        <p>
+        <p className="App-paragraph">
                 Click on the image and find the hidden NFTs.
                 <br/>
                 Create a MetaMask account and log in.
             </p>
-
-        {
-          isConnectedWeb3
-            ? <p><img src="https://cdn.worldvectorlogo.com/logos/metamask.svg" alt="logo_metamask" class="logo"></img></p>
-            : <button onClick={connectToWeb3}>Connect here</button>
-        }
-
         <img src={egg} alt="Easter egg hunt" width="612" height="408" border="0" usemap="#easter_eggs"></img>
             <map name="easter_eggs" id="easter_eggs">
-              <area shape="circle" coords="134,328,10" href="" alt="" onclick="alert('Oeuf n°1 trouvé !');" />
-              <area shape="circle" coords="193,341,10" href="" alt="" onclick="alert('Oeuf n°2 trouvé !');" />
-              <area shape="circle" coords="195,359,10" href="" alt="" onclick="alert('Oeuf n°3 trouvé !');" />
-              <area shape="circle" coords="250,348,5" href="" alt="" onclick="alert('Oeuf n°4 trouvé !');" />
-              <area shape="circle" coords="288,339,5" href="" alt="" onclick="alert('Oeuf n°5 trouvé !');" />
-              <area shape="circle" coords="478,315,5" href="" alt="" onclick="alert('Oeuf n°6 trouvé !');" />
-              <area shape="circle" coords="376,248,5" href="" alt="" onclick="alert('Oeuf n°7 trouvé !');" />
-              <area shape="circle" coords="481,252,10" href="" alt="" onclick="alert('Oeuf n°8 trouvé !');" />
-              <area shape="circle" coords="26,262,3" href="" alt="" onclick="alert('Oeuf n°9 trouvé !');" />
-              <area shape="circle" coords="100,242,10" href="" alt="" onclick="alert('Oeuf n°10 trouvé !');" />
-              <area shape="circle" coords="536,316,3" href="" alt="" onclick="alert('Oeuf n°11 trouvé !');" />
-              <area shape="circle" coords="551,308,3" href="" alt="" onclick="alert('Oeuf n°12 trouvé !');" />
-              <area shape="circle" coords="573,252,10" href="" alt="" onclick="alert('Oeuf n°13 trouvé !');" />
+              <area shape="circle" coords="134,328,10" href="" alt="" onClick="alert('Egg n°1 find!');" />
+              <area shape="circle" coords="193,341,10" href="" alt="" onClick="alert('Egg n°2 find!');" />
+              <area shape="circle" coords="195,359,10" href="" alt="" onClick="alert('Egg n°3 find!');" />
+              <area shape="circle" coords="250,348,5" href="" alt="" onClick="alert('Egg n°4 find!');" />
+              <area shape="circle" coords="288,339,5" href="" alt="" onClick="alert('Egg n°5 find!');" />
+              <area shape="circle" coords="478,315,5" href="" alt="" onClick="alert('Egg n°6 find!');" />
+              <area shape="circle" coords="376,248,5" href="" alt="" onClick="alert('Egg n°7 find!');" />
+              <area shape="circle" coords="481,252,10" href="" alt="" onClick="alert('Egg n°8 find!');" />
+              <area shape="circle" coords="26,262,3" href="" alt="" onClick="alert('Egg n°9 find!');" />
+              <area shape="circle" coords="100,242,10" href="" alt="" onClick="alert('Egg n°10 find!');" />
+              <area shape="circle" coords="536,316,3" href="" alt="" onClick="alert('Egg n°11 find!');" />
+              <area shape="circle" coords="551,308,3" href="" alt="" onClick="alert('Egg n°12 find!');" />
+              <area shape="circle" coords="573,252,10" href="" alt="" onClick="alert('Egg n°13 find!');" />
             </map>
       </header>
-      <footer>
+      
+      <footer className="App-footer">
             <p>Copyright &copy; The Easter Egg Hunt NFT Project, 2021 - All rights reserved | Directed by Fr&eacute;d&eacute;ric, S&eacute;bastien, Micka&euml;l &amp; Marjorie</p>
-            <p><a href="https://github.com/FredLC29/Visual_NFT_front/" target="_blank" rel="noreferrer"><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub" title="GitHub" class="logo_footer"/></a></p>
+            <p><a><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub" title="GitHub" class="logo_footer"/></a></p>
+            <p><a href="https://github.com/FredLC29/Visual_NFT_front/" target="_blank" rel="noreferrer" /></p>
+              
         </footer>
     </div>
   );
